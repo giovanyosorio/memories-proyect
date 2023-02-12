b288863e26748042e17ed36a0e15a2db3238dabf
giovanyosorio <giovany.torres.799@gmail.com>
2023-02-11 22:41:42 -0500

    memories project nodejs mongoose react express

---
+++ server/index.js
@@ -0,0 +1,6 @@
+import { express } from "express";
+import { bodyParser } from "body-parser";
+import {  Mongoose} from "mongoose";
+import { cors } from "cors";
+
+const express = require("express")
